ff3b4329aa1cb3c6f1cdac7a477642bc2de1f1e2
Shreyash Agrawal <35464275+sensei23@users.noreply.github.com>
2022-04-21 11:54:12 +0530

    Main menu control script added

--- SE-unit-3-new/Assets/Scripts/mainMenuControl.cs
+++ SE-unit-3-new/Assets/Scripts/mainMenuControl.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class mainMenuControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void StartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ResetGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame(){
+        Application.Quit();
     }
 }
